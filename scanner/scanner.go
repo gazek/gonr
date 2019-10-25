2059a7cfab26995c2357df25e3ee475d1066555a
greg <gregory.waliczek@gmail.com>
2019-10-25 16:17:06 -0700

    file scanner seems to work working through command line flags

---
+++ scanner/scanner.go
@@ -0,0 +1,64 @@
+package scanner
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"unicode"
+)
+
+// Scanner reads words from the input source, removes punctuation and lowercases the word
+type Scanner struct {
+	scanner  *bufio.Scanner
+	fileName string
+}
+
+// NewScanner creates a new scanner from a file path an error is returned if the path is invalid
+func NewScanner(f *os.File) *Scanner {
+	// create and return the Scanner struct
+	return newScanner(f)
+}
+
+func newScanner(r io.Reader) *Scanner {
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanWords)
+	return &Scanner{scanner: scanner}
+}
+
+// Next reads the returns the next word from the input source
+func (s *Scanner) Next() (word string, eof bool) {
+	// advance the scanner
+	eof = !s.scanner.Scan()
+	// check for end of file
+	if eof {
+		return "", true
+	}
+	// collect the new word
+	rawWord := s.scanner.Text()
+	// lowercase and remove punctuation
+	cleanWord := cleanWord(rawWord)
+	// if the cleaning resulted in an empty string
+	// then scan the next word by recursing
+	if cleanWord == "" {
+		return s.Next()
+	}
+	// return the cleaned word
+	return cleanWord, false
+}
+
+// cleanWord takes a string, lowercases it and removes punctuation
+func cleanWord(word string) string {
+	var cleaned string
+	for _, r := range []rune(word) {
+		// this will confuse a hyphen with a dash or double dash
+		// a dash or double dash is used for narative effect while
+		// a hyphon joins two words or splits words across a carriage return
+		// We would like to split on dash and double dash but drop a hyphen
+		// and join the words on either side of the hyphen
+		if unicode.IsPunct(r) {
+			continue
+		}
+		cleaned += string(unicode.ToLower(r))
+	}
+	return cleaned
+}
---
+++ scanner/scanner_test.go
@@ -0,0 +1,88 @@
+package scanner
+
+import (
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestCleanWord(t *testing.T) {
+	type test struct {
+		input string
+		want  string
+	}
+
+	tests := []test{
+		{input: "Foo", want: "foo"},
+		{input: "sHouldn't", want: "shouldnt"},
+		{input: "(foo)[bar]", want: "foobar"},
+		{input: "(foo)[bar]", want: "foobar"},
+		{input: "世界!!", want: "世界"},
+	}
+
+	for _, v := range tests {
+		if result := cleanWord(v.input); result != v.want {
+			t.Errorf("Got %v, wanted %v", result, v.want)
+		}
+	}
+}
+
+func TestnewScanner(t *testing.T) {
+	text := "foo! (bar) [baz]"
+	tests := strings.Split(text, " ")
+
+	s := newScanner(strings.NewReader(text))
+
+	// we'll test this by making sure that the created
+	// scanner in the Scanner struct returns the words from
+	// the sample string
+	for _, v := range tests {
+		s.scanner.Scan()
+		if result := s.scanner.Text(); result != v {
+			t.Errorf("Got %v, wanted %v", result, v)
+		}
+	}
+}
+
+func TestNext(t *testing.T) {
+	// again, at the expense of code duplication,
+	// I will this test seperate
+	text := "foo! (bar) ... [baz]"
+	tests := []string{"foo", "bar", "baz"}
+
+	s := newScanner(strings.NewReader(text))
+
+	// we'll test this by making sure that the created
+	// scanner in the Scanner struct returns the words from
+	// the sample string
+	for _, v := range tests {
+		if result, _ := s.Next(); result != v {
+			t.Errorf("Got %v, wanted %v", result, v)
+		}
+	}
+	// check EOF
+	if _, eof := s.Next(); eof != true {
+		t.Errorf("Failed tgo find EOF")
+	}
+}
+
+func TestNewScanner(t *testing.T) {
+	// this is awkward to test since it involves file I/O
+	// I'm just going to open the source file and make sure
+	// I see "package" as the first word scanned
+	path := "./scanner_test.go"
+	f, err := os.Open(path)
+	if err != nil {
+		t.Errorf("failed to open file for test: %v", path)
+	}
+	want := "package"
+	s := NewScanner(f)
+
+	// we'll test this by making sure that the created
+	// scanner in the Scanner struct returns the words from
+	// the sample string
+	s.scanner.Scan()
+	if result := s.scanner.Text(); result != want {
+		t.Errorf("Got %v, wanted %v", result, want)
+	}
+}
